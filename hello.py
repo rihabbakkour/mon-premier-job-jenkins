b1b42ebe4d744df07352f3dfd9c14907fdbf54d5
rihab bakkour <bakkourrihab4@gmail.com>
2026-05-19 12:32:38 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+#hello.py
+print("="*40)
+print("Bienvenue dans mon premier job jenkins")
+print("="*40)
+
+nom=input("Quel est ton nom?")
+print(f"bonjour {nom}, ton job Jenkins a reussi !")
+
+a=10
+b=5
+print(f"{a} + {b} = {a+b}")
+print(f"{a} - {b} ={a-b}")
